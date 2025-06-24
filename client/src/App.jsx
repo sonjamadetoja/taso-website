a868b8517d0615ccdb8171046f9898410c46a17b
Sonja Madetoja <sonja.madetoja@helsinki.fi>
2025-06-24 15:14:58 +0300

    front page

---
+++ client/src/App.jsx
@@ -0,0 +1,27 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <header>
+        <nav>
+          <img src="/logo.jpg" className="logo" alt="Logo" />
+          <h1>Taso Research</h1>
+        </nav>
+      </header>
+      <main>
+        <p>
+        Taso Reseach on tutkimusyritys, joka tarjoaa tutkimus- ja analyysipalveluja.
+Kokoamme asiakkaiden erityistarpeiden perusteella räätälöityjä työkaluja ja ratkaisuja.
+Asiantuntemuksemme perustuu tehtyyn tutkimukseen, kokemukseemme ​​tietojen analysoinnin, mallintamisen ja visualisoinnin alalta sekä jatkuvaan tuotekehitykseen.
+Yrityksemme edistää yhteiskuntavastuun ja ympäristön kannalta kestäviä päämääriä. Tekemiämme analyysejä ja tietomalleja käytetään esimerkiksi asiakkaiden strategisessa suunnittelussa, palvelujen kehittämisessä sekä vaikuttavuus- ja riskiarvioinnissa.
+        </p>
+      </main>
+      <footer>
+        © 2025 Taso Research. All rights reserved.
+      </footer>
+    </div>
+  )
+}
+
+export default App
