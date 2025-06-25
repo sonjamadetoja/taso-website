a4b55a0b6576bbf9e04c3efb567648e1a347711d
Sonja Madetoja <sonja.madetoja@helsinki.fi>
2025-06-25 19:44:24 +0300

    remove bullets from navigation; add LI to footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,18 +9,21 @@ function App() {
           <img src="/logo.jpg" className="logo" alt="Logo" />
           <h1>Taso Research</h1>
         <nav>
-          <ul>
-            <li><Link to={"/"}>Etusivu</Link></li>
-            <li><Link to={"/palvelut"}>Palvelut</Link></li>
-            <li><Link to={"/me"}>Me</Link></li>
-          </ul>
+          <Link to={"/"}>Etusivu   </Link>
+          <Link to={"/palvelut"}>Palvelut   </Link>
+          <Link to={"/me"}>Meistä </Link>
         </nav>
       </header>
       <main>
         <Outlet />
       </main>
       <footer>
-        © {d.getUTCFullYear()} Taso Research. All rights reserved.
+        <div>© {d.getUTCFullYear()} Taso Research. All rights reserved.</div>
+        <div>Seuraa meitä: 
+          <a href="https://www.linkedin.com/company/taso-research/" target="_blank" rel="noopener noreferrer">
+            <img src="/LI-In-Bug.png" alt="LinkedIn" width="24" height="24"/>
+          </a>
+        </div>
       </footer>
     </div>
   )
