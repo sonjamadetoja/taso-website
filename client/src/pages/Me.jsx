c6316ac6d4f8f9454e2d68c2fccc9ebada269fc6
Sonja Madetoja <sonja.madetoja@helsinki.fi>
2025-06-25 19:45:12 +0300

    add links to LI; remove P

--- client/src/pages/Me.jsx
+++ client/src/pages/Me.jsx
@@ -1,11 +1,23 @@
 function Me() {
   return (
     <div>
+      <h2>Meistä</h2>
       <ul>
-        <li>Jaakko Kaartinen, Research Director, MTh</li>
-        <li>Jarno Louhelainen, Lead Data Scientist</li>
-        <li>Jaakko Madetoja, Lead Spatial Data Scientist, PhD</li>
-        <li>Pauliina Vuorinen, Data Scientist, PhD</li>
+        <li>Jaakko Kaartinen, Research Director, MTh 
+          <a href="https://www.linkedin.com/in/jaakko-kaartinen-b8591019/" target="_blank" rel="noopener noreferrer">
+            <img src="/LI-In-Bug.png" alt="LinkedIn" width="24" height="24"/>
+          </a>
+        </li>
+        <li>Jarno Louhelainen, Lead Data Scientist 
+          <a href="https://www.linkedin.com/in/jarno-louhelainen/" target="_blank" rel="noopener noreferrer">
+            <img src="/LI-In-Bug.png" alt="LinkedIn" width="24" height="24"/>
+          </a>
+        </li>
+        <li>Jaakko Madetoja, Lead Spatial Data Scientist, PhD 
+          <a href="https://www.linkedin.com/in/jaakko-madetoja-06a022153/" target="_blank" rel="noopener noreferrer">
+            <img src="/LI-In-Bug.png" alt="LinkedIn" width="24" height="24"/>
+          </a>
+        </li>
       </ul>
     </div>
   )
